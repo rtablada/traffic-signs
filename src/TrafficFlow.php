05b1d43be11057da35873ee6be2a896513bc660c
Ryan Tablada <ryan.tablada@gmail.com>
2013-06-04 19:13:26 -0400

    All configged up!

--- src/TrafficFlow.php
+++ src/TrafficFlow.php
@@ -3,24 +3,9 @@
 App::error(function(Symfony\Component\HttpKernel\Exception\HttpException $e, $code)
 {
 	$headers = $e->getHeaders();
+	$errors = Config::get('traffic-signs::errors');
 
-	switch ($code)
-	{
-		case 401:
-			$message = 'Invalid API key';
-		break;
-
-		case 403:
-			$message = 'Insufficient privileges to perform this action';
-		break;
-
-		case 404:
-			$message = 'The requested resource was not found';
-		break;
-
-		default:
-			$message = 'An error was encountered';
-	}
+	$message = isset($errors[$code]) ? $errors[$code] : 'Something went wrong';
 
 	return View::make(Config::get('traffic-signs::view'), compact('code', 'message'));
 });
--- src/config/config.php
+++ src/config/config.php
@@ -6,7 +6,9 @@ return array(
 	 * in the view
 	 */
 	'errors' => array(
-
+		'404' => 'The requested resource was not found.',
+		'500' => 'There was a server error.',
+		'403' => 'You do not have the privileges to do this.'
 	),
 
 	'view' => 'traffic-signs::errors'
